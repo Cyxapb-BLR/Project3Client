ebe89147766911fec8af2fea673f3fa0914a9896
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-16 20:29:41 -0500

    Project3Client

---
+++ src/main/java/com/matskevich/springcourse/Project3Client/dto/SensorDTO.java
@@ -0,0 +1,20 @@
+package com.matskevich.springcourse.Project3Client.dto;
+
+public class SensorDTO {
+    private String name;
+
+    public SensorDTO() {
+    }
+
+    public SensorDTO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
